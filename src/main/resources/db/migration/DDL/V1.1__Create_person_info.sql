21a573dab0bf647161b572f72f404e8340b3c04f
Kollipara Tejkumar <ktejkum@l-hyd3zmg8wn-m.gap.com>
2018-02-06 16:59:55 +0530

    test commit

--- src/main/resources/db/migration/DDL/V1.1__Create_person_info.sql
+++ src/main/resources/db/migration/DDL/V1.1__Create_person_info.sql
@@ -6,10 +6,3 @@
   FIRST_NAME varchar2(20),
   LAST_NAME varchar2(20)
 );
-
- CREATE TABLE person_info_22
-(
-  PERSON_ID number(5) primary key,
-  FIRST_NAME varchar2(20),
-  LAST_NAME varchar2(20)
-);
